3f58f8719774a657c90b3897e6665af20785629c
Lalo <leyva958@gmail.com>
2017-08-14 10:37:36 -0500

    post with images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,11 @@
 class Post < ActiveRecord::Base
-	has_many :comments, dependent: :destroy
-	 has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-
 	validates :title, presence: true, length: {minimum: 3}
 	validates :body, presence: true	
+	
+	has_many :comments, dependent: :destroy
+	
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+
 end
