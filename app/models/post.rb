d17f60eda8ae219068f34ee0a8c423da94825139
Lalo <leyva958@gmail.com>
2017-08-14 09:33:29 -0500

    Pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
-	has_many :comments
+	has_many :comments, dependent: :destroy
+	 has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 	validates :title, presence: true, length: {minimum: 3}
 	validates :body, presence: true	
 end
